305fb6f758ce76e6d31c2cc337f903bfbeb15d74
Rubert_G <rubertgonzalez5@gmail.com>
2024-01-23 18:49:38 -0500

    👍 Basic connection with firebase

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,31 @@
-function Page () {
+import { collection, getDocs, query } from 'firebase/firestore'
+import { db } from './firebase/DBcore'
+
+async function Page () {
+  const q = query(collection(db, 'products'))
+  const querySnapshot = await getDocs(q)
+  let docs: any[] = []
+
+  querySnapshot.forEach((doc) => {
+    const data = { ...doc.data(), id: doc.id }
+    docs = [...docs, data]
+  })
+
   return (
-    <div>Page</div>
+    <div>
+      <h1>Products</h1>
+      <main>
+        {
+          docs.map((product) => {
+            return (
+              <article key={product.id}>
+                <h2>{product.title}</h2>
+              </article>
+            )
+          })
+        }
+      </main>
+    </div>
   )
 }
 

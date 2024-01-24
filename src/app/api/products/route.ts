5f127e621effc13c8df07cf4312f77850def3218
Rubert_G <rubertgonzalez5@gmail.com>
2024-01-24 17:30:07 -0500

    🛠️🆕 method GET for Products

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,1 +1,14 @@
-// categorias por -> ?
+import { getProducts } from '@/app/firebase/products-actions'
+import { NextResponse } from 'next/server'
+
+export async function GET () {
+  try {
+    const products = await getProducts()
+    return NextResponse.json(products)
+  } catch (error: any) {
+    return NextResponse.json({
+      message: 'Error obtaining the products',
+      error: error.message
+    })
+  }
+}

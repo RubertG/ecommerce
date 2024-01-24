8a37fb0905d2c85099a9b3e73a1797f7a6447da4
Rubert_G <rubertgonzalez5@gmail.com>
2024-01-24 17:12:20 -0500

    🛠️🆕 authState of know if it is authenticated

--- src/app/firebase/auth-actions.ts
+++ src/app/firebase/auth-actions.ts
@@ -1,9 +1,23 @@
-import { GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth'
+import { GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged } from 'firebase/auth'
 import { auth } from './DBcore'
 
 export const logout = async () => { await signOut(auth) }
 
 export const singInWithGoogle = async () => {
   const provider = new GoogleAuthProvider()
-  return await signInWithPopup(auth, provider)
+  await signInWithPopup(auth, provider)
+}
+
+export const authState = async () => {
+  return await new Promise((resolve, reject) => {
+    const unsubscribe = onAuthStateChanged(auth, (userCurrent) => {
+      if (userCurrent != null) {
+        const user = userCurrent
+        resolve(user)
+      } else {
+        resolve(null)
+      }
+      unsubscribe()
+    })
+  })
 }

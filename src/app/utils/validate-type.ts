f1c6c14af8a0acfda8bae31bf8d4a0c7eae259a1
Rubert_G <rubertgonzalez5@gmail.com>
2024-01-25 11:53:21 -0500

    🛠️🆕 Validates if an object has the expected shape with specific types for its properties

---
+++ src/app/utils/validate-type.ts
@@ -0,0 +1,42 @@
+/**
+ * Validates if an object has the expected shape with specific types for its properties.
+ *
+ * @param obj - The object to validate.
+ * @param expectedShape - An object that defines the expected types for each property of the object.
+ * @returns A boolean indicating whether the object matches the expected shape.
+ *
+ * @example
+ * ```typescript
+ * const user = {
+ *   id: '123',
+ *   name: 'John Doe',
+ *   age: 25,
+ * };
+ *
+ * const expectedShape = {
+ *   id: 'string',
+ *   name: 'string',
+ *   age: 'number',
+ * };
+ *
+ * if (validateObject(user, expectedShape)) {
+ *   console.log('The object is valid.');
+ * } else {
+ *   console.log('The object does not have the expected shape.');
+ * }
+ * ```
+*/
+
+export function validateObject<T> (obj: any, expectedShape: Record<keyof T, string | number | object>): obj is T {
+  for (const key in expectedShape) {
+    const expectedType = expectedShape[key]
+    if (!(key in obj)) {
+      return false
+    }
+    // eslint-disable-next-line valid-typeof
+    if (typeof obj[key] !== expectedType) {
+      return false
+    }
+  }
+  return true
+}

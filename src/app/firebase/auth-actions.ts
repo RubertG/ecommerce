3eecfb44a9f43afb5592cde785983fc8a681e994
Rubert_G <rubertgonzalez5@gmail.com>
2024-01-25 11:14:56 -0500

    🔧 Correction of the name and correction of the type returned by authState

--- src/app/firebase/auth-actions.ts
+++ src/app/firebase/auth-actions.ts
@@ -1,22 +1,23 @@
-import { GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged } from 'firebase/auth'
+import { GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged, type User } from 'firebase/auth'
 import { auth } from './DBcore'
 
-export const logout = async () => { await signOut(auth) }
+export const logout = async () => {
+  await signOut(auth)
+}
 
-export const singInWithGoogle = async () => {
+export const signInWithGoogle = async () => {
   const provider = new GoogleAuthProvider()
   await signInWithPopup(auth, provider)
 }
 
-export const authState = async () => {
+export const authState = async (): Promise<User | null> => {
   return await new Promise((resolve, reject) => {
     const unsubscribe = onAuthStateChanged(auth, (userCurrent) => {
       if (userCurrent != null) {
         const user = userCurrent
         resolve(user)
-      } else {
-        resolve(null)
       }
+      resolve(null)
       unsubscribe()
     })
   })
